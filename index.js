9f087c5c9b60f9d7e5a35ad050dffd798a054d81
Baibhav <baibhavtiwari37@gmail.com>
2025-02-15 20:52:11 +0530

    index.js(2)

--- index.js
+++ index.js
@@ -1,41 +1,51 @@
 require('dotenv').config();
 const { Web3 } = require('web3');
+const chalk = require('chalk');
 
-const infuraApiKey = process.env.INFURA_API_KEY;
+const infuraUrl = `https://mainnet.infura.io/v3/${process.env.INFURA_PROJECT_ID}`;
 
-const provideUrl = `https://mainnet.infura.io/v3/${infuraApiKey}`;
+const web3 = new Web3(infuraUrl);
 
-const web3 = new Web3(provideUrl);
+function formatTransactionDetails(transaction) {
+  return {
+    Hash: transaction.hash,
+    'Block Number': transaction.blockNumber,
+    From: transaction.from,
+    To: transaction.to,
+    Value: `${web3.utils.fromWei(transaction.value, 'ether')} ETH`,
+    Gas: transaction.gas,
+    'Gas Price': `${web3.utils.fromWei(transaction.gasPrice, 'gwei')} Gwei`,
+    Nonce: transaction.nonce,
+    'Input Data': transaction.input,
+  };
+}
 
-async function getTransactionDetails(txHash){
+function displayTransactionDetails(transaction) {
+  const details = formatTransactionDetails(transaction);
+  console.table(details);
+}
+
+async function getTransactionDetails(txHash) {
   try {
-    const transaction = await web3.eth.getTransaction(txHash);
-
-    if(!transaction) {
-      console.log('Transaction not Found.');
-      return;
+    if (!txHash || !web3.utils.isHexStrict(txHash)) {
+      throw new Error('Invalid transaction hash.');
     }
 
-    const transactionDetails = {
-      Hash: transaction.hash,
-      'Block Number' : transaction.blockNumber,
-      From: transaction.from,
-      To : transaction.to,
-      Value: `${web3.utils.fromWei(transaction.value, 'ether')} ETH`,
-      Gas: transaction.gas,
-      'Gas Price': `${web3.utils.fromWei(transaction.gasPrice, 'gwei')} Gwei`,
-      Nonce: transaction.nonce,
-      'Input Data' : transaction.input,
-      };
+    const transaction = await web3.eth.getTransaction(txHash);
+    if (!transaction) {
+      throw new Error('Transaction not found.');
+    }
 
-    console.table(transactionDetails);  
+    displayTransactionDetails(transaction);
+  } catch (error) {
+    console.error(chalk.red('Error:'), error.message);
   }
 }
 
 const txHash = process.argv[2];
-if(!txHash) {
-  console.log('please provide a transaction Hash.');
+if (!txHash) {
+  console.log(chalk.red('Please provide a transaction hash.'));
   process.exit(1);
 }
 
-gettransactionDetails(txHash);
+getTransactionDetails(txHash);
